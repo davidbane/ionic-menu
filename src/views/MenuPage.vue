ccb64b4c0c18b15e9a336a6f4f9b6cc1cbee2144
dbane18 <dtbane18@gmail.com>
2023-08-23 21:20:24 -0500

    feat: updates tab layout

--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -2,17 +2,17 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Menu</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Menu</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Menu" />
     </ion-content>
   </ion-page>
 </template>

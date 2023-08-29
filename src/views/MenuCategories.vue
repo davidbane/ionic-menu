1b60e05be74893a2aa4ac4d0315fcc98e393cfe6
dbane18 <dtbane18@gmail.com>
2023-08-28 21:39:23 -0500

    feat: adds click menu list

---
+++ src/views/MenuCategories.vue
@@ -0,0 +1,22 @@
+<template>
+	<ClickMenuList 
+    title="Categories"
+    :items="items"
+	/>
+</template>
+
+<script lang="ts">
+	import { defineComponent } from 'vue';
+	import ClickMenuList from '@/components/ClickMenuList.vue';
+
+	export default defineComponent({
+		components: {
+			ClickMenuList
+		},
+		data() {
+			return { 
+				items: [{name: 'category1'}, {name: 'category2'}]
+			}
+		},
+	});
+</script>
